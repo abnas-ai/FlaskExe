c3cecedf7e7779418f7152aa45cfc4bc0bd58d60
Kipngeno Abednego <abnasgit@gmail.com>
2025-04-27 12:28:09 +0300

    created new routes

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
 from . import app
-from .forms import TaskForm
-from .models import Task
-from flask import render_template, redirect, url_for, flash
+from .forms import TaskForm, UserForm
+from .models import Task, User
+from flask import render_template, flash, redirect, url_for
 from . import db
+
 
 
 @app.route('/')
 def home():
-    return "Welcome to the Task Manager!" 
-    
+    users = User.query.all()
+    user_count = User.query.count()
+    tasks = Task.query.all()
+    task_count = Task.query.count()
+    return render_template('index.html', title='Home', users=users, tasks=tasks, user_count=user_count,  task_count=task_count)
     
 @app.route('/create-task', methods=['GET', 'POST'])
 def create_task():
@@ -23,10 +27,19 @@ def create_task():
         db.session.add(new_task)
         db.session.commit()
         flash('Task created successfully!')
-        return redirect(url_for('create_task'))
-    return render_template('forms.html', form=form)
+    return render_template('tasks.html', task_form=form)
 
 @app.route('/tasks')
 def tasks():
-    tasks = Task.query.all()
-    return render_template('forms.html', tasks=tasks)
+   
+    form = TaskForm()
+    
+    tasks = Task.query.order_by(Task.timestamp.desc()).all()
+    return render_template('tasks.html', tasks=tasks, task_form=form, title='Tasks')
+
+
+@app.route('/users')
+def users():
+    form = UserForm()
+    
+    return render_template('users.html', users=users, title='Users', user_form=form)
